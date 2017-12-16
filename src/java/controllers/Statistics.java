9e46b785c3a61a581ac9c0bb0f1fb34e0bca6b6a
yasserotiefy <co2yasser@gmail.com>
2017-12-16 21:02:19 +0200

    edit files

--- src/java/controllers/Statistics.java
+++ src/java/controllers/Statistics.java
@@ -10,12 +10,13 @@ package controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Question;
 
 /**
  *
